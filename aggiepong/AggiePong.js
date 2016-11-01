c54d05acc9de2489c7c71b6315ba2e60a4352fe1
Dakota Sloniger <sloniger@tamu.edu>
2016-11-01 17:20:49 -0500

    Add files via upload

---
+++ aggiepong/AggiePong.js
@@ -0,0 +1,117 @@
+
+var game = new Phaser.Game(600, 800, Phaser.AUTO, 'Aggie Pong', {preload: preload, create: create, update: update, render: render });
+
+function preload() {
+	
+	game.load.image('centerline', 'assets/centerline.png');
+	game.load.image('ball', 'assets/ball.png');
+	game.load.image('paddle', 'assets/paddle.png');
+	
+}
+
+var centerline;
+var balls;
+var paddle;
+var cursors;
+
+function create() {
+	
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	
+	centerline = game.add.group();
+	centerline.enableBody = true;
+	
+	for (var x = 0; x < 10; x++){
+		
+		var dash = centerline.create((x * 100) -37, game.world.centerY, 'centerline');
+		//centerline.body.immovable = true;
+		
+	}
+	
+	paddle = game.add.sprite(game.world.centerX - 60, 730, 'paddle');
+	//paddle.anchor.setTo()
+	
+	game.physics.enable(paddle, Phaser.Physics.ARCADE);
+	
+	paddle.body.collideWorldBounds = true;
+	paddle.body.bounce.set(1);
+	paddle.body.immovable = true;
+	
+	
+	balls = game.add.group();
+	balls.enableBody = true;
+	
+	ball = game.add.sprite((Math.random() * 595), game.world.centerY - 12, 'ball');
+	//ball.anchor.set(0.5);
+	ball.checkWorldBounds = true;
+	
+	game.physics.enable(ball, Phaser.Physics.ARCADE);
+	
+	ball.body.collideWorldBounds = true;
+	ball.body.bounce.set(1);
+	
+	cursors = game.input.keyboard.createCursorKeys();
+}
+	
+function update() {
+	
+	game.physics.arcade.collide(ball, paddle, ballHitPaddle, null, this);
+	paddle.body.velocity.x = 0;
+	paddle.body.velocity.y = 0;
+	
+	if(cursors.left.isDown && !cursors.right.isDown){
+		paddle.body.velocity.x = -750;
+	}
+	if(cursors.right.isDown && !cursors.left.isDown){
+		paddle.body.velocity.x = 750;
+	}
+	
+	if(cursors.up.isDown){
+		paddle.body.bounce.set(1.2);
+	}
+	if(!cursors.up.isDown){
+		paddle.body.bounce.set(1);
+	}
+	
+	
+	
+}
+	
+function render(){
+	
+}
+	
+
+function ballHitPaddle(_ball, _paddle) {
+	
+	var diff = 0;
+	
+	//ball is on left-hand side
+	if (_ball.x < _paddle.x){
+		
+		diff = _paddle.x - _ball.x;
+		_ball.body.velocity.x = (diff * -10);
+		
+	}
+	
+	//ball is on right-hand side
+	else if (_ball.x > _paddle.x){
+		
+		diff = _ball.x - _paddle.x;
+		_ball.body.velocity.x = (diff * 10);
+		
+	}
+	//ball is perfectly in the middles
+	else{
+		_ball.body.velocity.x = 2 + Math.random() * 6;
+	}
+	
+}
+
+//************create ball function, and ball velocity
+function createBall() {
+	
+	
+}
+
+
