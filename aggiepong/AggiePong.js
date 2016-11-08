1723c6254ebbd3f4393ef2ece7ffb585ea341b22
Dakota Sloniger <sloniger@tamu.edu>
2016-11-08 10:07:51 -0600

    the logic for bombs is implimented

--- aggiepong/AggiePong.js
+++ aggiepong/AggiePong.js
@@ -14,28 +14,42 @@ function preload() {
 
 var centerline;
 var balls;
-var paddle;
+var paddle_1;
+var paddle_2;
 var player_1pts = 0;
 var player_2pts = 0;
 var cursors;
+var keyA;
+var keyD;
 
 var timer;
 var milliseconds = 0;
 var seconds = 0;
 var minutes = 0;
 
-var score;
+var score_1;
+var score_2;
 var counter = 0;
 var ball_direction = 1;
+
+var bomb_counter = 4;
+var bombs;
+var p1_stunned = false;
+var p2_stunned = false;
 
 
 function create() {
 	
 	timer = game.add.bitmapText(250, 250, 'carrier', '00:00:00');
-	score = game.add.bitmapText(32, 32, 'carrier', '0');
+	score_1 = game.add.bitmapText(32, 16, 'carrier', 'Score: 0');
+	score_2 = game.add.bitmapText(600 - 200, 800 - 32, 'carrier', 'Score: 0');
+	score_1.scale.setTo(.5, .5);
+	score_2.scale.setTo(.5, .5);
+	
 	
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 	game.physics.arcade.checkCollision.down = false;
+	game.physics.arcade.checkCollision.up = false;
 	
 	centerline = game.add.group();
 	centerline.enableBody = true;
@@ -47,39 +61,58 @@ function create() {
 		
 	}
 	
-	paddle = game.add.sprite(game.world.centerX - 60, 730, 'paddle');
-	game.physics.enable(paddle, Phaser.Physics.ARCADE);
+	paddle_1 = game.add.sprite(game.world.centerX - 60, 730, 'paddle');
+	paddle_2 = game.add.sprite(game.world.centerX - 60, 70, 'paddle');
+	game.physics.enable(paddle_1, Phaser.Physics.ARCADE);
+	game.physics.enable(paddle_2, Phaser.Physics.ARCADE);
 	
-	paddle.body.collideWorldBounds = true;
-	paddle.body.immovable = true;
-	paddle.anchor.setTo(.5, .5);
-	
+	paddle_1.body.collideWorldBounds = true;
+	paddle_1.body.immovable = true;
+	paddle_1.anchor.setTo(.5, .5);
+	paddle_2.body.collideWorldBounds = true;
+	paddle_2.body.immovable = true;
+	paddle_2.anchor.setTo(.5, .5);
 	
 	balls = game.add.group();
+	bombs = game.add.group();
+	balls.add(bombs);
 	balls.enableBody = true;
 	balls.checkWorldBounds = true;
+	bombs.enableBody = true;
+	bombs.checkWorldBounds = true;
 	
-
 	game.time.events.loop(Phaser.Timer.SECOND * 3, createBall, this);
 	
-	
-	
 	cursors = game.input.keyboard.createCursorKeys();
+	keyA = game.input.keyboard.addKey(Phaser.Keyboard.A);
+	keyD = game.input.keyboard.addKey(Phaser.Keyboard.D);
 	
 }
 	
 function update() {
 	
 	updateTimer();
-	game.physics.arcade.collide(paddle, balls, ballHitPaddle, null, this);
-	paddle.body.velocity.x = 0;
-	paddle.body.velocity.y = 0;
+	game.physics.arcade.collide(paddle_1, balls, ballHitpaddle, null, this);
+	game.physics.arcade.collide(paddle_2, balls, ballHitpaddle, null, this);
+	game.physics.arcade.collide(paddle_1, bombs, ballHitpaddle, null, this);
+	game.physics.arcade.collide(paddle_2, bombs, ballHitpaddle, null, this);
+	paddle_1.body.velocity.x = 0;
+	paddle_1.body.velocity.y = 0;
+	paddle_2.body.velocity.x = 0;
+	paddle_2.body.velocity.y = 0;
 	
-	if(cursors.left.isDown && !cursors.right.isDown){
-		paddle.body.velocity.x = -750;
+	if(cursors.left.isDown && !cursors.right.isDown && !p1_stunned){
+		paddle_1.body.velocity.x = -750;
 	}
-	if(cursors.right.isDown && !cursors.left.isDown){
-		paddle.body.velocity.x = 750;
+	if(cursors.right.isDown && !cursors.left.isDown && !p1_stunned){
+		paddle_1.body.velocity.x = 750;
+	}
+	
+	if(keyA.isDown && !keyD.isDown && !p2_stunned){
+		paddle_2.body.velocity.x = -750;
+	}
+	if(keyD.isDown && !keyA.isDown && !p2_stunned){
+			paddle_2.body.velocity.x = 750;
 	}
 	
 	//impliment smacking the ball, increasing its velocity
@@ -97,10 +130,9 @@ function render(){
 }
 	
 
-function ballHitPaddle(_paddle, _ball) {
+function ballHitpaddle(_paddle, _ball) {
 	
 	var diff = 0;
-	
 	
 	//ball is on left-hand side
 	if (_ball.x < _paddle.x){
@@ -127,14 +159,29 @@ function ballHitPaddle(_paddle, _ball) {
 //create ball function, and ball velocity
 function createBall() {
 	
-	var ball = balls.create((Math.random() * 595), game.world.centerY - 12, 'ball');
-	ball.body.velocity.setTo(Math.random() * 200 - 100, (Math.random() * 200 + 400) * ball_direction);
-	ball.checkWorldBounds = true;
-	ball.body.bounce.set(1);
-	ball.body.collideWorldBounds = true;
-	ball.anchor.setTo(.5, .5);
-	ball.events.onOutOfBounds.add(function(){playerScored(ball)}, this);
-	ball_direction *= -1;
+	if(bomb_counter < 4){
+		var ball = balls.create((Math.random() * 595), game.world.centerY - 12, 'ball');
+		ball.body.velocity.setTo(Math.random() * 200 - 100, (Math.random() * 200 + 400) * ball_direction);
+		ball.checkWorldBounds = true;
+		ball.body.bounce.set(1);
+		ball.body.collideWorldBounds = true;
+		ball.anchor.setTo(.5, .5);
+		ball.events.onOutOfBounds.add(function(){playerScored(ball)}, this);
+		ball_direction *= -1;
+		bomb_counter++;
+	}
+	
+	else{
+		var bomb = bombs.create((Math.random() * 595), game.world.centerY - 12, 'bomb');
+		bomb.body.velocity.setTo(Math.random() * 200 - 100, (Math.random() * 200 + 400) * ball_direction);
+		bomb.checkWorldBounds = true;
+		bomb.body.bounce.set(1);
+		bomb.body.collideWorldBounds = true;
+		bomb.anchor.setTo(.5, .5);
+		bomb.events.onOutOfBounds.add(function(){bombMissed(bomb)}, this);
+		ball_direction *= -1;
+		bomb_counter = 0;
+	}
 }
 
 
@@ -163,13 +210,38 @@ function playerScored(_ball){
 	
 	if(_ball.y < 50){
 		player_2pts++;
+		score_2.setText('Score: ' + player_2pts);
 	}
 	else {
 		player_1pts++;
+		score_1.setText('Score: ' + player_1pts);
 	}
 	
-	score.setText('Score: ' + player_1pts + ' ' + player_2pts);
-	
 	_ball.kill();
+	
+}
+
+function bombMissed(_bomb){
+	
+	if(_bomb.y < 50){
+		p2_stunned = true;
+		game.time.events.add(Phaser.Timer.SECOND * 2, this.stunTimer, this, 2);
+	}
+	else {
+		p1_stunned = true;
+		game.time.events.add(Phaser.Timer.SECOND * 2, this.stunTimer, this, 1);
+	}
+}
+
+function stunTimer(player){
+	if(player == 1){
+		p1_stunned = false;
+	}
+	else{
+		p2_stunned = false;
+	}
+}
+
+function time(){
 	
 }
